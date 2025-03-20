832d580204c4a17f10a5adc975c794b0932c0794
AIT EL HAJ Abdelouadoud <abdelouadoud.aitelhaj.mailme@example.com>
2025-03-20 22:15:38 +0000

    Format.

--- .config/ags/applauncher/app.ts
+++ .config/ags/applauncher/app.ts
@@ -3,7 +3,7 @@ import style from "./style.scss"
 import Applauncher from "./widget/Applauncher"
 
 App.start({
-    instanceName: "launcher",
-    css: style,
-    main: Applauncher,
+  instanceName: "launcher",
+  css: style,
+  main: Applauncher,
 })
